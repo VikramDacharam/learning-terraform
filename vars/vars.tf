5f251ad656774a59c1a7ddf886165d87bbf05966
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-07-23 20:04:27 +0530

    aa

--- vars/vars.tf
+++ vars/vars.tf
@@ -4,3 +4,18 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+# String variables
+
+variable "Sample1" {
+  default = "Hello world"
+}
+
+# numbers variables
+variable "Sample2" {
+  default = 100
+}
+# boolean variables
+variable "Sample3" {
+  default = true
+}
