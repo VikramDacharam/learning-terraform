fd33f765a83a68364b35c42e2d4cb1c62c9e1361
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-07-24 11:22:28 +0530

    aa

--- resources/main.tf
+++ resources/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   instance_type = "t3.micro"
-  ami           = ami-077bb1f989e30b12c
+  ami           = "ami-077bb1f989e30b12c"
 }
 provider "aws" {
   region = "us-east-1"
