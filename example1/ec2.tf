971b6df9930c9dd122ce679f6413825cc6646018
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-07-25 13:14:40 +0530

    aa

---
+++ example1/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "sample" {
+  instance_type = "t3.micro"
+  ami           = "ami-077bb1f989e30b12c"
+}
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.14.0"
+    }
+  }
+}
