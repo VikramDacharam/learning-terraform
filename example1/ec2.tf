58a65e7abbfcbcbd067b467758a546946c654bdf
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-07-25 13:30:53 +0530

    aa

--- example1/ec2.tf
+++ example1/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample" {
   instance_type = "t3.micro"
   ami           = "ami-077bb1f989e30b12c"
+  vpc_security_group_ids = [aws_security_group.allow_ec2.id]
 }
 provider "aws" {
   region = "us-east-1"
