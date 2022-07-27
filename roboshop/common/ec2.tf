593779d33a2de0d591cdd21684d60d1f14123b95
Vikram Dacharam <vikramdacharam95@gmail.com>
2022-07-27 22:12:14 +0530

    aa

--- roboshop/common/ec2.tf
+++ roboshop/common/ec2.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "ec2" {
   ami                    = var.AMI
   vpc_security_group_ids = [aws_security_group.allow_ec2.id]
 }
-  tags = {
+  tag = {
     Name = var.COMPONENT
   }
